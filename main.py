2e55312c90ab3ef0baa6226243ff51d8eeb8c999
nate7611 <natedowneymain@gmail.com>
2026-09-10 14:48:47 -0400

    feat: add run_menu_option

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+def add_book():
+    pass
+
+
+def remove_book():
+    pass
+
+
+def list_books():
+    pass
+
+
+def run_menu_option(menu_option):
+    match menu_option:
+        case 1:
+            add_book()
+        case 2:
+            remove_book()
+        case 3:
+            list_books()
+        case 4:
+            print("Goodbye")
+        case _:
+            print("Unknown menu option, exiting.")
+
+
 def select_menu_option():
     option_number = 0
     while True:
@@ -8,7 +34,7 @@ def select_menu_option():
                 option_number = int(input("Select option: "))
             except:
                 print("Input must be a valid number")
-    print(option_number)
+    run_menu_option(option_number)
 
 
 def display_menu():
@@ -18,11 +44,11 @@ def display_menu():
     print("3. List books")
     print("4. Exit")
     select_menu_option()
-    
-    
+
+
 def main():
     display_menu()
-    
-    
+
+
 if __name__ == "__main__":
     main()
