1ea65aa5a9a02da5383d3b7d649aa9f919550f28
nate7611 <natedowneymain@gmail.com>
2026-09-10 14:34:19 -0400

    feat: add main funtion and menu display

---
+++ main.py
@@ -0,0 +1,12 @@
+def display_menu():
+    print("\nPersonal Library Manager")
+    print("1. Add book")
+    print("2. Remove book")
+    print("3. List books")
+    print("4. Exit")
+
+def main():
+    display_menu()
+    
+if __name__ == "__main__":
+    main()
