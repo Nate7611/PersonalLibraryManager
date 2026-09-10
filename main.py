b85c9ebc5b61ca6021dadb632b3eddc14298bae9
nate7611 <natedowneymain@gmail.com>
2026-09-10 14:43:11 -0400

    feat: implement select_menu_option

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+def select_menu_option():
+    option_number = 0
+    while True:
+        if option_number > 0 and option_number < 5:
+            break
+        else:
+            try:
+                option_number = int(input("Select option: "))
+            except:
+                print("Input must be a valid number")
+    print(option_number)
+
+
 def display_menu():
     print("\nPersonal Library Manager")
     print("1. Add book")
     print("2. Remove book")
     print("3. List books")
     print("4. Exit")
-
+    select_menu_option()
+    
+    
 def main():
     display_menu()
+    
     
 if __name__ == "__main__":
     main()
